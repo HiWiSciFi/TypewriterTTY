d40d028e5bac8886221533acb047f90320e9032c
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2025-01-17 23:14:47 +0100

    sefe

--- Raspi/Keymap.hpp
+++ Raspi/Keymap.hpp
@@ -18,7 +18,7 @@ struct KeymapKey {
     int out;
     uint8_t mod;
 
-    bool operator<(const KeymapKey& rhs) {
+    bool operator<(const KeymapKey rhs) {
         return 
             (scan < rhs.scan)
             || (scan == rhs.scan && out < rhs.out)
