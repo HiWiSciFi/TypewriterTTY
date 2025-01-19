e5e436cf10f6b0781eadb21094e3442d1632f2e4
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2025-01-19 19:10:46 +0100

    no input

--- Raspi/main.cpp
+++ Raspi/main.cpp
@@ -117,7 +117,10 @@ int main(int argc, char** argv) {
 
     while (true) {
         auto key = readKey();
-        if (key.codepoint == 0 && key.key == 0) continue;
+        if (key.codepoint == 0 && key.key == 0) {
+            std::cout << "no input" << std::endl;
+            continue;
+        }
         std::cout << "Key U+" << std::setfill('0') << std::setw(6) << std::right << std::hex << key.codepoint
                   << " ASCII 0x" << std::setfill('0') << std::setw(2) << std::right << std::hex << key.key << std::endl;
     }
