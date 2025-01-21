fed8da0fccf9be6ca451981e5416d83413f44272
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2025-01-21 22:26:42 +0100

    test3 update

--- Raspi/main.cpp
+++ Raspi/main.cpp
@@ -120,7 +120,8 @@ int main(int argc, char** argv) {
         for (int m = 0; m < (sizeof(pinsScan) / sizeof(*pinsScan)); m++) {
             digitalWrite(pinsScan[m], LOW);
             delay(1);
-            for (int i = 1; i < (sizeof(pinsOut) / sizeof(*pinsOut)); i++) {
+            for (int i = 0; i < (sizeof(pinsOut) / sizeof(*pinsOut)); i++) {
+                if (i == 0 && m == 1) continue;
                 if (digitalRead(pinsOut[i]) == LOW) {
                     std::cout << m << ", " << i << " pressed" << std::endl;
                 }
