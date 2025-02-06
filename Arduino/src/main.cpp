bbfd8cb21fceac8cbc9ec10d69134d7807aaf73f
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2025-02-06 12:36:49 +0100

    added ACK response to arduino

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -7,6 +7,9 @@ uint8_t outPorts[8] = { 21, 20, 19, 18, 17, 16, 15, 14 };
 // uint8_t scanPorts[9] = { 45, 46, 47, 48, 49, 50, 51, 52, 53 };
 // uint8_t outPorts[8] = { 34, 35, 36, 37, 38, 39, 40, 41 };
 
+#define ASCII_ACK 0x06
+#define ASCII_NAK 0x15
+
 #define ASIZE(arr) (sizeof( arr ) / sizeof( arr[0] ))
 
 // TODO: consider using INPUT_PULLUP
@@ -24,18 +27,7 @@ void setup() {
 void loop() {
   while (Serial.available() > 0) {
     uint8_t c = (uint8_t)Serial.read();
-    Serial.print("Received: 0x");
-    Serial.println(c, HEX);
-    switch (c) {
-    case '\n':
-      c = KEY_RETURN;
-      break;
-    case '\r':
-      return;
-    case 0x17:
-      c = KEY_DEL_WORD;
-      break;
-    }
+    Serial.print(ASCII_ACK);
     writeKey(c);
   }
 
