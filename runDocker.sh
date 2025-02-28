35f3bb3ea72605a737c40b40d1447bbd036315c6
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2025-02-28 11:22:11 +0100

    removed rm flag

--- runDocker.sh
+++ runDocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm -h tty.int --device /dev/mem --device /dev/gpiomem --device /dev/ttyACM0 --restart always typewritertty
+sudo docker run -h tty.int --device /dev/mem --device /dev/gpiomem --device /dev/ttyACM0 --restart always typewritertty
